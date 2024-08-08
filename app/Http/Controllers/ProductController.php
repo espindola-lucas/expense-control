0c18b6236e5536bedb95b063bdc40858c748226f
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2024-08-08 17:01:05 -0300

    Add small changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends Controller
         $formattedRestMoney = number_format($restMoney, 0, '', '.');
         $formattedTotalPrice = number_format($totalPrice, 0, '', '.');
         $lastConfiguration = $this->getLastConfiguration();
-    
+
         $years = range(2024, 2030);
         $months = $this->getMonths();
     
@@ -108,9 +108,6 @@ class ProductController extends Controller
     }
 
     private function getStartDateFromDatabase($user) {
-        // $userId = Auth::id();
-        
-        // Obtener la última configuración para el usuario actual
         $configuration = Configuration::where('user_id', $user->id)
                                       ->latest()
                                       ->first();
@@ -148,9 +145,15 @@ class ProductController extends Controller
     }
     
     private function getFilteredProductsByDateRange($startDate, $endDate){
-        return Product::whereBetween('expense_date', [$startDate, $endDate])
-                        ->orderBy('expense_date')
-                        ->get();
+        $products = Product::whereBetween('expense_date', [$startDate, $endDate])
+                            ->orderBy('expense_date')
+                            ->get();
+        
+        foreach($products as $product){
+            $product->price = number_format($product->price, 0, '', '.');
+        }
+
+        return $products;
     }
 
     private function getAvailableMoney($userId, $startDate = null, $endDate = null) {
@@ -236,32 +239,7 @@ class ProductController extends Controller
     }
     
     private function getMonths() {
-        $monthsTranslation = [
-            'January' => 'Enero',
-            'February' => 'Febrero',
-            'March' => 'Marzo',
-            'April' => 'Abril',
-            'May' => 'Mayo',
-            'June' => 'Junio',
-            'July' => 'Julio',
-            'August' => 'Agosto',
-            'September' => 'Septiembre',
-            'October' => 'Octubre',
-            'November' => 'Noviembre',
-            'December' => 'Diciembre'
-        ];
-    
-        $months = collect([]);
-        for ($month = 1; $month <= 12; $month++) {
-            $monthName = \DateTime::createFromFormat('!m', $month)->format('F');
-            $translatedMonthName = $monthsTranslation[$monthName];
-            $months->push((object)[
-                'name' => $translatedMonthName,
-                'value' => str_pad($month, 2, '0', STR_PAD_LEFT)
-            ]);
-        }
-    
-        return $months;
+        return \Helps::getNameMonths();;
     }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,12 @@ GET /products/{product}/edit → ProductController@edit
 PUT/PATCH /products/{product} → ProductController@update
 DELETE /products/{product} → ProductController@destroy
 */
-Route::middleware('auth')->group(function () {
+
+Route::middleware([
+    'auth',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
     Route::get('/dashboard', [ProductController::class, 'index'])->name('dashboard');
     Route::resource('products', ProductController::class);
     Route::resource('configuration', ConfigurationController::class);
