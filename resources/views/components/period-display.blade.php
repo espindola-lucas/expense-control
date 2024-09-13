b79048670018fd5394c91cf4127fb85b454bdd98
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2024-09-13 16:56:59 -0300

    Refactor

---
+++ resources/views/components/period-display.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/components/period-display.blade.php -->
+@unless(!$lastConfiguration)
+    @unless(is_null($lastConfiguration->start_counting) || is_null($lastConfiguration->end_counting))
+        <div class="text-center text-black py-2 px-4">Periodo: {{ $lastConfiguration->start_counting }} - {{ $lastConfiguration->end_counting }}</div>
+    @elseif(!is_null($lastConfiguration->start_counting))
+        <div class="text-center text-black py-2 px-4">Periodo: {{ $lastConfiguration->start_counting }} - Sin corte</div>
+    @else
+        <div class="text-center text-black py-2 px-4">Periodo: Sin inicio - {{ $lastConfiguration->end_counting }}</div>
+    @endunless
+@endunless
