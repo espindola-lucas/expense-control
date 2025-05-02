ec629c9766441b9218da8b122a598f7318ace935
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2025-05-02 15:23:26 -0300

    Closes #113 se crean metodos para ver el query ejecutado y ademas el contenido del mismo

--- app/helpers/Helps.php
+++ app/helpers/Helps.php
@@ -2,6 +2,10 @@
 namespace App\Helpers;
 use Carbon\Carbon;
 use App\Enums\MonthEnum;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use function PHPUnit\Framework\throwException;
 
 class Helps{
     public static function getNameMonths(){
@@ -28,6 +32,41 @@ class Helps{
             $branch = trim(str_replace('ref: refs/heads/', '', $headContent));
         }
         return $branch;
+    }
+
+    public static function saveQuery($query, string $filename = 'consulta-sql')
+    {
+
+        // get SQL and bindings according to the query type
+        if ($query instanceof EloquentBuilder || $query instanceof QueryBuilder) {
+            $sql = $query->toSql();
+            $bindings =  $query->getBindings();
+        } else {
+            throw new \InvalidArgumentException('Expected instance of Query\Builder or Eloquent\Builder');
+        }
+
+        foreach ($bindings as $binding) {
+            $value = is_numeric($binding) ? $binding : "'" . addslashes($binding) . "'";
+            $sql = preg_replace('/\?/', $value, $sql, 1);
+        }
+
+        $path = storage_path("logs/{$filename}.txt");
+
+        File::append($path, $sql . PHP_EOL);
+    }
+
+    public static function executeLogQuery($query, string $filename = 'resultados-query'){
+        if ($query instanceof EloquentBuilder || $query instanceof QueryBuilder){
+            $results = $query->get();
+        } else {
+            throw new \InvalidArgumentException('Expected instance of Query\Builder or Eloquent\Builder');
+        }
+
+        // convert results to readable JSON
+        $jsonData = $results->toJson(JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE);
+
+        $path = storage_path("logs/{$filename}.json");
+        File::put($path, $jsonData);
     }
 }
 
