e1c818aa526a2df112992baf5f38e4753879e3af
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2025-06-22 01:13:28 -0300

    Change background colour

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -13,8 +13,8 @@
         @vite('resources/css/app.css')
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen flex bg-page-custom flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-semi-white shadow-md overflow-hidden sm:rounded-lg">
                 <form method="POST" action="{{ route('session-register.store') }}">
                 @csrf
                     <div>
