d5e065f07c6cbdf360a338267706c4498b32ca51
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2026-06-25 16:33:26 -0300

    add new name for route

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [SessionAuthController::class, 'logout']);
     Route::get('/user', fn(Request $request) => $request->user());
     Route::apiResource('spents', SpentApiController::class)
-        ->only(['index', 'store', 'update', 'destroy']);
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('api.spents');
     Route::apiResource('personal-configurations', PersonalConfigurationApiController::class)
         ->only(['index', 'store', 'update', 'destroy']);
 });
